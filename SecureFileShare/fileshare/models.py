6a7820ab4b5fbcd04be58add3096f29850094a58
Ajit Maurya <mauryaajit.am@outlook.com>
2023-12-29 21:31:38 +0530

    created models

--- SecureFileShare/fileshare/models.py
+++ SecureFileShare/fileshare/models.py
@@ -1,3 +1,13 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+
+class UploadedFile(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    file = models.FileField(upload_to='uploads/')
+    file_type = models.CharField(max_length=10,choices=[('pptx','pptx'),('docx','docx'),('xlsx','xlsx')])
+
+class ClientUserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    verification_code = models.CharField(max_length=100)
